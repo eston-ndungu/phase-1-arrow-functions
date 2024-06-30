0565bcfa132be38be5ee8944d4011e00fa1b2e97
ESTON NDUNGU <estonndungu1@gmail.com>
2024-06-30 10:12:08 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,18 @@
+//Declare a function using a function expression
+const divide = function() {
+    let a = 2000
+    let b = 100
+    return a / b
+
+}
+
+//Declare a function using arrow functions
+
+const square = (parameter1) => parameter1 * parameter1
+
+//Arrow function takes two parameters
+const add = (parameter1, parameter2) => parameter1 + parameter2
+
+
+
+
